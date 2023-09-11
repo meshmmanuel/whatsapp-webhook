fbddd7aff4cf4d47e29211d98ee528f2d13f8f82
mesh <meshmmanuel@yahoo.com>
2023-09-11 17:49:42 +0100

    remove gitignore files

---
+++ server.js
@@ -0,0 +1,22 @@
+// Import required modules
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Create an Express app
+const app = express();
+
+// Use bodyParser to parse JSON requests
+app.use(bodyParser.json());
+
+// Define a route to handle incoming webhook requests
+app.post("/webhook", (req, res) => {
+  // Respond with a simple message
+  res.json({ message: "Hello, Webhook!" });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
